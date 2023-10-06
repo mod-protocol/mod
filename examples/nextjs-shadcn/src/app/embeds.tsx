ae3aeb5f5df7a97af078a46397c5d56798614bd7
David Furlong <dvfurlong@googlemail.com>
2023-10-06 17:54:13 +0200

    fix: give end user control over render miniapps

--- examples/nextjs-shadcn/src/app/embeds.tsx
+++ examples/nextjs-shadcn/src/app/embeds.tsx
@@ -4,12 +4,22 @@ import React from "react";
 import { renderers } from "@mod-protocol/react-ui-shadcn/dist/renderers";
 import { RenderEmbed } from "@mod-protocol/react";
 import { Embed } from "@mod-protocol/core";
+import {
+  fallbackRenderMiniApp,
+  renderMiniApps,
+} from "@mod-protocol/miniapp-registry";
 
 export function Embeds(props: { embeds: Array<Embed> }) {
   return (
     <div>
       {props.embeds.map((embed, i) => (
-        <RenderEmbed embed={embed} key={i} renderers={renderers} />
+        <RenderEmbed
+          embed={embed}
+          key={i}
+          renderers={renderers}
+          fallbackRenderMiniApp={fallbackRenderMiniApp}
+          renderMiniApps={renderMiniApps}
+        />
       ))}
     </div>
   );
