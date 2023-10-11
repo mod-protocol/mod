d47e3e6e6ca401360de9843b21db626a1c974899
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-10-11 14:46:22 +0200

    feat: resolve NFT chain icons

---
+++ examples/api/src/lib/chains/chain-icon.ts
@@ -0,0 +1,35 @@
+import { chainById } from "./chain-index";
+
+export async function resolveChainIcon(chainId: number) {
+  const chain = await fetch(
+    `https://raw.githubusercontent.com/ethereum-lists/chains/master/_data/chains/eip155-${chainId}.json`
+  );
+  if (!chain.ok) {
+    return null;
+  }
+  const chainJson = await chain.json();
+
+  const iconRes = await fetch(
+    `https://raw.githubusercontent.com/ethereum-lists/chains/master/_data/icons/${chainJson.icon}.json`
+  );
+  if (!iconRes.ok) {
+    return null;
+  }
+  const icons = await iconRes.json();
+  let { url } = icons[0];
+
+  if (url) {
+    if (url.startsWith("ipfs://")) {
+      url = url.replace(
+        "ipfs://",
+        "https://raw.githubusercontent.com/ethereum-lists/chains/master/_data/iconsDownload/"
+      );
+    }
+  } else {
+    // Fall back to alternative asset db, might want to check if it actually exists before returning
+    const chain = chainById[chainId];
+    url = `https://raw.githubusercontent.com/DefiLlama/icons/master/assets/agg_icons/${chain.network}.jpg`;
+  }
+
+  return url;
+}
