7d652306bd1ada5956a189eaedeef0b194d9513b
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-11-02 12:13:41 +0100

    feat(opengraph): handle image urls without making external calls

--- examples/api/src/app/api/open-graph/lib/url-handlers/index.ts
+++ examples/api/src/app/api/open-graph/lib/url-handlers/index.ts
@@ -3,8 +3,8 @@ import caip19 from "./caip-19";
 import fallback from "./fallback";
 import opensea from "./opensea";
 import zora from "./zora";
-import imgur from "./imgur";
+import imageFileUrl from "./image-file";
 
-const handlers: UrlHandler[] = [opensea, zora, caip19, imgur, fallback];
+const handlers: UrlHandler[] = [opensea, zora, caip19, imageFileUrl, fallback];
 
 export default handlers;
