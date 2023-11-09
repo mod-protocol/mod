f934eec8085f05a00580437ae31cbe15ed28c6b1
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-11-09 11:40:01 +0300

    feat(api): postgres serverless connection pooling

--- examples/api/src/app/api/cast-embeds-metadata/lib/db.ts
+++ examples/api/src/app/api/cast-embeds-metadata/lib/db.ts
@@ -1,25 +1,17 @@
 import { Kysely, PostgresDialect } from "kysely";
 import { DB } from "../types/db";
+import { createKysely } from "@vercel/postgres-kysely";
 import { Pool } from "pg";
 
-// Client is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
-//
-// Learn more:
-// https://pris.ly/d/help/next-js-best-practices
-
-const globalForDb = global as unknown as {
-  db: Kysely<DB> | undefined;
-};
-
 export const db =
-  globalForDb.db ??
-  new Kysely<DB>({
-    dialect: new PostgresDialect({
-      pool: new Pool({
+  process.env.NODE_ENV !== "production"
+    ? new Kysely<DB>({
+        dialect: new PostgresDialect({
+          pool: new Pool({
+            connectionString: process.env.DATABASE_URL,
+          }),
+        }),
+      })
+    : createKysely<DB>({
         connectionString: process.env.DATABASE_URL,
-      }),
-    }),
-  });
-
-if (process.env.NODE_ENV !== "production") globalForDb.db = db;
+      });
