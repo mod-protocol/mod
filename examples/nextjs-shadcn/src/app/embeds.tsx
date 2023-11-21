0405e05c3aa996974b9f9fb16b47f798b715f803
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-11-21 17:48:39 +0200

    fix(example): useMemo for embed context and resolvers

--- examples/nextjs-shadcn/src/app/embeds.tsx
+++ examples/nextjs-shadcn/src/app/embeds.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Embed } from "@mod-protocol/core";
+import { ContextType, Embed } from "@mod-protocol/core";
 import {
   contentMiniApps,
   defaultContentMiniApp,
@@ -12,55 +12,65 @@ import {
   switchNetwork,
   waitForTransaction,
 } from "@wagmi/core";
+import { useMemo } from "react";
 import { useAccount } from "wagmi";
 
 export function Embeds(props: { embeds: Array<Embed> }) {
   const { address } = useAccount();
 
+  const onSendEthTransactionAction = useMemo(
+    () =>
+      async ({ data, chainId }, { onConfirmed, onError, onSubmitted }) => {
+        try {
+          const parsedChainId = parseInt(chainId);
+
+          // Switch chains if the user is not on the right one
+          await switchNetwork({ chainId: parsedChainId });
+
+          // Send the transaction
+          const { hash } = await sendTransaction({
+            ...data,
+            chainId: parsedChainId,
+          });
+          onSubmitted(hash);
+
+          // Wait for the transaction to be confirmed
+          const { status } = await waitForTransaction({
+            hash,
+            chainId: parsedChainId,
+          });
+
+          onConfirmed(hash, status === "success");
+        } catch (e) {
+          onError(e);
+        }
+      },
+    []
+  );
+
+  const context = useMemo<Omit<ContextType, "embed">>(() => {
+    return {
+      api: process.env.NEXT_PUBLIC_API_URL,
+      user: {
+        wallet: {
+          address,
+        },
+      },
+    };
+  }, [address]);
+
   return (
     <div>
       {props.embeds.map((embed, i) => (
         <RenderEmbed
-          api={process.env.NEXT_PUBLIC_API_URL}
           embed={embed}
+          {...context}
           key={i}
           renderers={renderers}
           defaultContentMiniApp={defaultContentMiniApp}
           contentMiniApps={contentMiniApps}
-          user={{
-            wallet: {
-              address,
-            },
-          }}
           resolvers={{
-            onSendEthTransactionAction: async (
-              { data, chainId },
-              { onConfirmed, onError, onSubmitted }
-            ) => {
-              try {
-                const parsedChainId = parseInt(chainId);
-
-                // Switch chains if the user is not on the right one
-                await switchNetwork({ chainId: parsedChainId });
-
-                // Send the transaction
-                const { hash } = await sendTransaction({
-                  ...data,
-                  chainId: parsedChainId,
-                });
-                onSubmitted(hash);
-
-                // Wait for the transaction to be confirmed
-                const { status } = await waitForTransaction({
-                  hash,
-                  chainId: parsedChainId,
-                });
-
-                onConfirmed(hash, status === "success");
-              } catch (e) {
-                onError(e);
-              }
-            },
+            onSendEthTransactionAction,
           }}
         />
       ))}
