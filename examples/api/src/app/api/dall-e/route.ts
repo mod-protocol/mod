beb01d147e9f2c338d3709ce9944f5025df3294f
David Furlong <dvfurlong@googlemail.com>
2023-12-05 16:02:21 +0100

    chore: reupload to imgur

--- examples/api/src/app/api/dall-e/route.ts
+++ examples/api/src/app/api/dall-e/route.ts
@@ -1,4 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
+const { NEXT_PUBLIC_IMGUR_CLIENT_ID } = process.env;
+
+async function fetchBlob(url) {
+  const response = await fetch(url);
+  return response.blob();
+}
+
+const uploadToImgur = async (file: Blob): Promise<string | null> => {
+  const formData = new FormData();
+  formData.append("image", file);
+  formData.append("type", "file");
+  const response = await fetch("https://api.imgur.com/3/upload", {
+    method: "POST",
+    headers: {
+      Authorization: `Client-ID ${NEXT_PUBLIC_IMGUR_CLIENT_ID}`, // replace with your Client ID
+    },
+    body: formData,
+  });
+
+  const { data, error } = await response.json();
+
+  if (!response.ok) {
+    console.error(response.status, response.statusText, error, data);
+    return null;
+  }
+
+  if (!data) return null;
+
+  return data.link;
+};
+
+const genRanHex = (size) =>
+  [...Array(size)]
+    .map(() => Math.floor(Math.random() * 16).toString(16))
+    .join("");
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
@@ -15,13 +50,18 @@ export async function POST(request: NextRequest) {
       body: JSON.stringify({
         model: "dall-e-3",
         prompt: body.prompt,
+        response_format: "url",
+        // We're not going to be able to credibly store anonymized user ids
+        // OpenAI can see these are never used twice, and are hence likely randomized like this, but whether they will act on that is tbd
+        user: `user_${genRanHex(16)}`,
       }),
     }
   );
 
   if (!imageResponse.ok) {
+    console.error(imageResponse.status, imageResponse.statusText);
     return NextResponse.json(
-      { message: "Something went wrong" },
+      { message: "Something went wrong, try again later" },
       { status: imageResponse.status }
     );
   }
@@ -31,8 +71,28 @@ export async function POST(request: NextRequest) {
     data: Array<{ url: string; revised_prompt: string }>;
   } = await imageResponse.json();
 
+  let urls;
+  try {
+    urls = await Promise.all(
+      responseBody.data.map(async (el) => {
+        const b = await fetchBlob(el.url);
+        const imgur = await uploadToImgur(b);
+        if (!imgur) throw new Error("couldnt upload to imgur");
+        return imgur;
+      })
+    );
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
   return NextResponse.json({
-    response: responseBody,
+    response: {
+      ...responseBody,
+      data: responseBody.data.map((el, i) => ({
+        url: urls[i],
+        revised_prompt: el.revised_prompt,
+      })),
+    },
   });
 }
 
