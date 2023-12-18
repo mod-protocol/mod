e1d2bf9eb97ecff6849971bb89db492c475e7e4a
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-12-18 16:05:00 +0200

    fix(react-ui-shadcn): text renderer overflow

--- packages/react-ui-shadcn/src/renderers/text.tsx
+++ packages/react-ui-shadcn/src/renderers/text.tsx
@@ -3,7 +3,7 @@ import { Renderers } from "@mod-protocol/react";
 import { cva } from "class-variance-authority";
 import { cn } from "../lib/utils";
 
-const textVariants = cva("my-0 flex-1 text-sm", {
+const textVariants = cva("my-0 flex-1 text-sm break-words", {
   variants: {
     variant: {
       bold: "font-bold",
