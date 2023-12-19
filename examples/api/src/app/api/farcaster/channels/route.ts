6ae4441d7d331885fedb0e706846143e6a8a153e
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-12-19 11:17:34 +0200

    fix(api): fetch channels from neynar API

--- examples/api/src/app/api/farcaster/channels/route.ts
+++ examples/api/src/app/api/farcaster/channels/route.ts
@@ -1,25 +1,41 @@
-import { NextResponse, NextRequest } from "next/server";
-import channels from "./channels.json";
-
-// Update Channels from: https://github.com/neynarxyz/farcaster-channels/blob/main/warpcast.json
-
-const channelsWithHome = [
-  {
-    name: "Home",
-    parent_url: null,
-    image: "https://warpcast.com/~/channel-images/home.png",
-    channel_id: "home",
-  },
-  ...channels.sort((a, b) => (a.name < b.name ? -1 : 1)),
-];
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const q = searchParams.get("q");
 
+  const channelsRes = await fetch(
+    "https://api.neynar.com/v2/farcaster/channel/list",
+    {
+      headers: {
+        api_key: process.env.NEYNAR_API_SECRET,
+      },
+    }
+  );
+  const result = await channelsRes.json();
+
+  const channels = result.channels.map((channel) => ({
+    name: channel.name,
+    parent_url: channel.url,
+    image: channel.image_url,
+    channel_id: channel.id,
+  }));
+
+  const channelsWithHome = [
+    {
+      name: "Home",
+      parent_url: null,
+      image: "https://warpcast.com/~/channel-images/home.png",
+      channel_id: "home",
+    },
+    ...channels.sort((a, b) => (a.name < b.name ? -1 : 1)),
+  ];
+
   return NextResponse.json({
-    channels: channelsWithHome.filter((channel) =>
-      channel.name.toLowerCase().includes(q.toLowerCase())
-    ),
+    channels: !!q
+      ? channelsWithHome.filter((channel) => {
+          return channel.name.toLowerCase().includes(q.toLowerCase());
+        })
+      : channelsWithHome,
   });
 }
