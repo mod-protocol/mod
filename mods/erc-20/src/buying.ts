26b59c33d41358bf564e65c53403765523ca4784
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2024-01-15 17:18:53 +0200

    feat: buy tokens

---
+++ mods/erc-20/src/buying.ts
@@ -0,0 +1,72 @@
+import { ModElement } from "@mod-protocol/core";
+
+const buy: ModElement[] = [
+  {
+    type: "padding",
+    elements: [
+      {
+        type: "horizontal-layout",
+        elements: [
+          {
+            type: "circular-progress",
+          },
+          {
+            type: "text",
+            label:
+              "Buying ~${{refs.buyAmountUsd}} of {{refs.tokenReq.response.data.tokenData.name}}...",
+          },
+        ],
+      },
+
+      {
+        type: "horizontal-layout",
+        elements: [
+          // If there is no buyAmountUsd value, go to #view
+          {
+            if: {
+              value: "{{refs.buyAmountUsd}}",
+              match: {
+                equals: "",
+              },
+            },
+            then: {
+              type: "horizontal-layout",
+              onload: "#view",
+            },
+          },
+        ],
+      },
+      {
+        type: "horizontal-layout",
+        onload: {
+          type: "POST",
+          url: "{{api}}/erc-20/buy?walletAddress={{user.wallet.address}}&token={{embed.url}}&buyAmountUsd={{refs.buyAmountUsd}}",
+          ref: "swapTxDataReq",
+          onsuccess: {
+            type: "SENDETHTRANSACTION",
+            ref: "swapTxReq",
+            txData: {
+              from: "{{refs.swapTxDataReq.response.data.transaction.from}}",
+              to: "{{refs.swapTxDataReq.response.data.transaction.to}}",
+              value: "{{refs.swapTxDataReq.response.data.transaction.value}}",
+              data: "{{refs.swapTxDataReq.response.data.transaction.data}}",
+            },
+            chainId: "{{refs.balancesReq.response.data.chain.id}}",
+            onerror: {
+              type: "SETSTATE",
+              ref: "buyAmountUsd",
+              value: "",
+            },
+          },
+          onerror: {
+            type: "SETSTATE",
+            ref: "buyAmountUsd",
+            value: "",
+          },
+        },
+      },
+    ],
+  },
+];
+
+export default buy;
